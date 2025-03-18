e090bcaa782c271a6c18fe083b989fa78ad7d910
RaulRv007 <116015516+RaulRv007@users.noreply.github.com>
2025-03-18 17:49:18 -0400

    coral led added

--- src/main/java/frc/robot/subsystems/Led.java
+++ src/main/java/frc/robot/subsystems/Led.java
@@ -3,23 +3,30 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants;
-
 
 
 public class Led extends SubsystemBase {
   /** Creates a new ExampleSubsystem. **/
+  CommandXboxController m_Controller = new CommandXboxController(0);
 
   private AddressableLED m_led;
   private AddressableLEDBuffer m_ledBuffer;
   private DutyCycleEncoder encoderAngle = new DutyCycleEncoder(0);
-  final int seperation = 30;//led hue
+  private Trigger is_coral = m_Controller.leftBumper();
+
+  final int seperation = 0;//led hue
   final int not_selected = 0;//led hue
-  final int in_position = 60;//led hue
+  final int in_position = 30;//led hue
   final int not_in_position = 90;//led hue
   int position = not_in_position;//led hue
+  int coral_in = not_in_position;//led hue
   private static final int LENGTH = 73;
   final int segment_size = (LENGTH/5);
   int lvl_4_Angle = 45;
@@ -139,10 +146,12 @@ public class Led extends SubsystemBase {
   }
   public void Coral() {
     // For every pixel
-    if((encoderAngle.get()*360) >= lvl_0_Angle - threshold&& (encoderAngle.get()*360) <= lvl_0_Angle + threshold) {
-      position = in_position;
+    if(is_coral.getAsBoolean()) {
+      coral_in = in_position;
+      System.out.println("no");
     } else {
-      position = not_in_position;
+      coral_in = not_in_position;
+      System.out.println("yea");
     } 
 
     for (var j = 0; j < segment_size; j+=1) {
@@ -150,7 +159,7 @@ public class Led extends SubsystemBase {
     // m_ledBuffer.setHSV((3*segment_size)+j, not_selected, 255, 32);
     // m_ledBuffer.setHSV((2*segment_size)+j, not_selected, 255, 32);
     // m_ledBuffer.setHSV((segment_size)+j, not_selected, 255, 32);
-    m_ledBuffer.setHSV(j, position, 255, 32);
+    m_ledBuffer.setHSV(j, coral_in, 255, 32);
 }
     // m_ledBuffer.setHSV((segment_size), seperation, 255, 32);
     // m_ledBuffer.setHSV((2*segment_size), seperation, 255, 32);
